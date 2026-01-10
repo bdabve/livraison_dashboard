74a8452c38e7a79b5589f38382dc745006ebe966
bdabve <bdabve@github.com>
2026-01-10 14:51:25 +0100

    some changes

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,8 @@
 # ----------------------------------------------------------------------------
 
 import pandas as pd
-# import plotly.express as px
+import utils
+import plotly.express as px
 import streamlit as st
 
 # Page configuration
@@ -25,120 +26,153 @@ def load_data_from_excel():
         if col in df.columns:
             df[col] = pd.to_numeric(df[col], errors="coerce")
 
+    # df["OBSERVATION"] = df["OBSERVATION"].astype(str)
     # Remove rows without a valid DATE (e.g. subtotal / footer rows)
-    clean_df = df[df["DATE"].notna()]
-    clean_df = clean_df.fillna(0)
-    return clean_df
+    df = df[df["DATE"].notna()]
+    df = df.fillna(0)
+    return df
 
 
+# Load data
 df = load_data_from_excel()
 
-# --- Side Bar
-st.sidebar.header('Filter Here:')
+# ------- Side Bar -------
+st.sidebar.header('Filtré:')
 
 livreur = st.sidebar.multiselect(
-    'Select the Livreur:',
+    'LIVREUR:',
     options=df['LIVREUR'].unique(),
     default=df['LIVREUR'].unique()
 )
 
-# customer_type = st.sidebar.multiselect(
-    # 'Select the Customer Type:',
-    # options=df['Customer_type'].unique(),
-    # default=df['Customer_type'].unique()
-# )
-
-# gender_info = st.sidebar.multiselect(
-    # 'Select the Gender:',
-    # options=df['Gender'].unique(),
-    # default=df['Gender'].unique()
-# )
-
+# Filter data based on selection
 df_selection = df.query('LIVREUR == @livreur')
 
-# TOP
-import utils
+# ----------------------------------------------------
+# Etat Journalier
+# ---------------
 etat_excel = utils.etat_excel_like_db(df_selection)
 
-accompte_column, credit_column, versement_column, charge_column = st.columns(4)
-
+st.title(':bar_chart: Livraison Dashboard')
+st.markdown('##')
+accompte_column, credit_column = st.columns(2)
 with accompte_column:
-    st.subheader('ACCOMPTE:')
-    st.subheader(f" $ {etat_excel['ACCOMPTE']:,}")
+    st.subheader(f'ACCOMPTE: $ {etat_excel["ACCOMPTE"]:,}')
 
 with credit_column:
-    st.subheader('CREDIT:')
-    st.subheader(f'$ {etat_excel["CREDIT"]:,}')
+    st.subheader(f'CREDIT: $ {etat_excel["CREDIT"]:,}')
 
+versement_column, charge_column = st.columns(2)
 with versement_column:
-    st.subheader('VERS. CREDIT:')
-    st.subheader(f'$ {etat_excel["VERS. CREDIT"]:,}')
+    st.subheader(f'VERS. CREDIT: $ {etat_excel["VERS. CREDIT"]:,}')
 
 with charge_column:
-    st.subheader('CHARGES:')
-    st.subheader(f'$ {etat_excel["CHARGES"]:,}')
+    st.subheader(f'CHARGES: $ {etat_excel["CHARGES"]:,}')
 
 st.markdown('---')
 
 # Display the daily report
 fields = ["T. COMMANDE", "T.LOGICIEL", "VERSEMENT", "CHARGE", "DIFF"]
 etat_journalier = utils.etat_journalier(df, fields)
-print(etat_journalier)
 st.subheader("Etat Journalier")
-st.dataframe(etat_journalier, column_config={"DATE": st.column_config.DateColumn("DATE", format="DD-MM-YYYY")})
+st.dataframe(
+    etat_journalier,
+    column_config={"DATE": st.column_config.DateColumn("DATE", format="DD-MM-YYYY")},
+    hide_index=True
+)
 
 st.markdown('---')
+st.space()
+st.subheader("Versement by Date")
+# Plotly Bar Chart for VERSEMENT
+df_plot = etat_journalier[etat_journalier["Date"] != "TOTAL"]
+fig_versement = px.bar(
+    df_plot,
+    x="Date",
+    y="VERSEMENT",
+    orientation="v",
+    title="<b>Versement Par Jour</b>",
+    template="plotly_white",
+    color_discrete_sequence=["#0083B8"],
+)
+# remove bg color for plot
+fig_versement.update_layout(
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis=(dict(showgrid=False))
+)
+# Display the charts
+st.plotly_chart(fig_versement, use_container_width=True)
+
+
+# ----------------------------------------------------
+# Etat Total par Livreur
+# ------------------------
+sum_by_driver = utils.sum_by_driver(df_selection, fields)
+st.subheader("Etat Total par Livreur")
+st.dataframe(
+    sum_by_driver,
+    column_config={"DATE": st.column_config.DateColumn("DATE", format="DD-MM-YYYY")},
+)
+print(sum_by_driver)
+
+# Graphique Versement par Livreur
+fig_livreur = px.bar(
+    sum_by_driver,
+    x=sum_by_driver.index,
+    y="VERSEMENT",
+    orientation="v",
+    title="<b>Versement Par Livreur</b>",
+    template="plotly_white",
+    color_discrete_sequence=["#0083B8"],
+)
+# remove bg color for plot
+fig_livreur.update_layout(
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis=(dict(showgrid=False))
+)
+# Display the charts
+st.plotly_chart(fig_livreur, use_container_width=True)
+
+# Versement Livreur Pourcentage
+sum_by_driver = sum_by_driver.reset_index()
+total_versement = sum_by_driver["VERSEMENT"].sum()
+sum_by_driver["VERSEMENT %"] = (sum_by_driver["VERSEMENT"] / total_versement * 100).round(2)
+
+table_pourcent, plot_pourcent = st.columns(2)
+with table_pourcent:
+    st.subheader("Pourcentage de Versement par Livreur")
+    st.dataframe(
+        sum_by_driver[["LIVREUR", "VERSEMENT", "VERSEMENT %"]],
+        column_config={"DATE": st.column_config.DateColumn("DATE", format="DD-MM-YYYY")},
+        hide_index=True
+    )
+
+with plot_pourcent:
+    fig_pourcent = px.pie(
+        sum_by_driver,
+        names="LIVREUR",
+        values="VERSEMENT %",
+        title="<b>Pourcentage de Versement par Livreur</b>",
+        template="plotly_white",
+    )
+    st.plotly_chart(fig_pourcent, use_container_width=True)
+
+# left_column, right_column = st.columns(2)
+# left_column.plotly_chart(fig_versement, use_container_width=True)
 
 # Display the dataframe
-st.dataframe(df, column_config={"DATE": st.column_config.DateColumn("DATE", format="DD-MM-YYYY")})
-
-# Display the plot
-# sales_by_product_line = (df_selection.groupby(by=['Product line']).sum(numeric_only=True)[['Total']].sort_values(by="Total"))
-
-# fig_product_sales = px.bar(
-    # sales_by_product_line,
-    # x='Total',
-    # y=sales_by_product_line.index,
-    # orientation='h',
-    # title='<b>Sales By Product Line.</b>',
-    # color_discrete_sequence=['#0083B8'] * len(sales_by_product_line),
-    # template='plotly_white',
-# )
-# remove bg color for plot
-# fig_product_sales.update_layout(
-    # plot_bgcolor='rgba(0,0,0,0)',
-    # xaxis=(dict(showgrid=False))
-# )
-
-# Sales by hour
-# sales_by_hour = df_selection.groupby(by=['hour']).sum(numeric_only=True)[['Total']]
-
-# fig_hourly_sales = px.bar(
-    # sales_by_hour,
-    # x=sales_by_hour.index,
-    # y='Total',
-    # title='<b>Sales By Hour.</b>',
-    # color_discrete_sequence=['#0083B8'] * len(sales_by_product_line),
-    # template='plotly_white',
-# )
-
-# fig_hourly_sales.update_layout(
-    # xaxis=dict(tickmode='linear'),
-    # plot_bgcolor='rgba(0,0,0,0)',
-    # yaxis=(dict(showgrid=False))
-# )
-
-# Display the charts in 2 columns
-# left_column, right_column = st.columns(2)
-# left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-# right_column.plotly_chart(fig_product_sales, use_container_width=True)
+st.subheader("Raw Data")
+st.dataframe(
+    df,
+    column_config={"DATE": st.column_config.DateColumn("DATE", format="DD-MM-YYYY")}
+)
 
 # hide some stylesheet
-hide_st_style = '''
-<style>
-    #MainMenu { visibility: hidden; }
-    header { visibility: hidden; }
-    footer { visibility: hidden; }
-</style>
-'''
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = '''
+# <style>
+#     #MainMenu { visibility: hidden; }
+#     header { visibility: hidden; }
+#     footer { visibility: hidden; }
+# </style>
+# '''
+# st.markdown(hide_st_style, unsafe_allow_html=True)
--- utils.py
+++ utils.py
@@ -37,13 +37,28 @@ def etat_journalier(clean_df, fields):
     :fields: list of fields to sum
     """
     # --- SUM By Date
-    daily_stats = clean_df.groupby("DATE")[fields[:-1]].sum()       # without observation
+    daily_stats = clean_df.groupby("DATE")[fields].sum()       # without observation
     daily_stats = daily_stats.sort_values(by="DATE", ascending=True)
-    daily_stats.index = daily_stats.index.date  # Convert to date only
+    # daily_stats.index = daily_stats.index.date  # Convert to date only
 
     # Create totals row
     total_row = pd.DataFrame(daily_stats.sum()).T
     total_row.index = ["TOTAL"]
     # Append totals inside table
     etat_journalier = pd.concat([daily_stats, total_row])
+    etat_journalier.index.name = "Date"
     return etat_journalier.reset_index()
+
+
+def sum_by_driver(clean_df, fields):
+    """
+    ETAT TOTAL BY LIVREUR
+    clean_df: DataFrame
+    fields: list of fields to sum
+    """
+    # --- TOTAL PAR LIVREUR SUMMARY ---
+    driver_stats = clean_df.groupby("LIVREUR", as_index=False)[fields].sum()
+    driver_stats = driver_stats[driver_stats["LIVREUR"].isin(["AMINE", "TOUFIK", "REDA", "MOHAMED"])]
+    driver_stats = driver_stats.sort_values(by="VERSEMENT", ascending=False)
+    driver_stats = driver_stats.set_index("LIVREUR")
+    return driver_stats
